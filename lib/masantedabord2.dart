f2601c8ea55f929f954d3ad09a533429a9169ba8
TECHNICIEN CHERCHEUR <chercheurtechnicien4@gmail.com>
2022-02-01 10:22:47 +0100

    nouveau

--- lib/masantedabord2.dart
+++ lib/masantedabord2.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class scrollables2 extends StatefulWidget {
@@ -8,47 +9,121 @@ class scrollables2 extends StatefulWidget {
 }
 
 class _scrollables2State extends State<scrollables2> {
-  // List<Categories> lesCategories = [
-
-  // ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blueAccent,
           leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 7),
-              child: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.medical_services_rounded),
+          actions: [Icon(Icons.more_vert_outlined)],
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              height: 630,
+              decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      topLeft: Radius.circular(30))),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 28,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 170,
+                        height: 180,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        width: 170,
+                        height: 180,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Container(
+                          height: 80,
+                          width: 70,
+                          decoration: BoxDecoration(
+                            color: Colors.amber,
+                            // image: DecorationImage(
+                            //     image: AssetImage(
+                            //       "vato/images/TikenJah.jpg",
+                            //     ),
+                            //     fit: BoxFit.cover),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 170,
+                        height: 180,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        width: 170,
+                        height: 180,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 170,
+                        height: 180,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        width: 170,
+                        height: 180,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             )
           ],
-        ),
-        body: Center(
-          child: ListView.builder(
-              itemCount: 11,
-              itemBuilder: (context, i) {
-                return ListTile(
-                  title: Text("Papa je t'aime $i"),
-                );
-              }),
         ));
   }
 }
-
-// class Categories {
-//   String nom;
-//   IconData icone;
-
-//   Categories(
-//     String nom,
-//     IconData icone,
-//   ) {
-//     this.nom = nom;
-//     this.icone = icone;
-//   }
-// }
