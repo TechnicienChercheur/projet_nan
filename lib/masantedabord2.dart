d8b9cbb3f858a8033f1851f6c4189cf94720d8b8
iMac-50 <imac-50@iMac-de-iMac-50.local>
2022-02-01 16:47:54 +0000

    du nouveau

--- lib/masantedabord2.dart
+++ lib/masantedabord2.dart
@@ -13,15 +13,87 @@ class _scrollables2State extends State<scrollables2> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          elevation: 0,
           backgroundColor: Colors.blueAccent,
           leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
-          actions: [Icon(Icons.more_vert_outlined)],
+          actions: [
+            PopupMenuButton(
+              icon: Icon(Icons.more_vert),
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Settings",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      Icon(
+                        Icons.settings,
+                        color: Colors.blueAccent,
+                      )
+                    ],
+                  ),
+                ),
+                PopupMenuItem(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Share",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      Icon(
+                        Icons.share,
+                        color: Colors.blueAccent,
+                      )
+                    ],
+                  ),
+                ),
+                PopupMenuItem(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Learn more",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      Icon(
+                        Icons.info,
+                        color: Colors.blueAccent,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
+            Padding(
+              padding:
+                  const EdgeInsets.only(bottom: 9, right: 9, left: 9, top: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    "vato/images/img7.png",
+                    height: 130,
+                  ),
+                  Text(
+                    "Choisir une catégorie\npour commencer",
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
             Container(
-              height: 630,
+              height: 450,
               decoration: BoxDecoration(
                   color: Colors.blueAccent,
                   borderRadius: BorderRadius.only(
@@ -30,7 +102,7 @@ class _scrollables2State extends State<scrollables2> {
               child: Column(
                 children: [
                   SizedBox(
-                    height: 28,
+                    height: 40,
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -39,8 +111,21 @@ class _scrollables2State extends State<scrollables2> {
                         width: 170,
                         height: 180,
                         decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10)),
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              "vato/images/img2.png",
+                              height: 150,
+                            ),
+                            Text(
+                              "Soins de santé",
+                              style: TextStyle(fontSize: 20),
+                            )
+                          ],
+                        ),
                       ),
                       SizedBox(
                         width: 20,
@@ -52,18 +137,31 @@ class _scrollables2State extends State<scrollables2> {
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        child: Container(
-                          height: 80,
-                          width: 70,
-                          decoration: BoxDecoration(
-                            color: Colors.amber,
-                            // image: DecorationImage(
-                            //     image: AssetImage(
-                            //       "vato/images/TikenJah.jpg",
-                            //     ),
-                            //     fit: BoxFit.cover),
-                          ),
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              "vato/images/img4.png",
+                              height: 150,
+                            ),
+                            Text(
+                              "Carie dentaire",
+                              style: TextStyle(fontSize: 20),
+                            )
+                          ],
                         ),
+
+                        // child: Container(
+                        //   height: 80,
+                        //   width: 70,
+                        //   decoration: BoxDecoration(
+                        //     color: Colors.amber,
+                        //     // image: DecorationImage(
+                        //     //     image: AssetImage(
+                        //     //       "vato/images/TikenJah.jpg",
+                        //     //     ),
+                        //     //     fit: BoxFit.cover),
+                        //   ),
+                        // ),
                       ),
                     ],
                   ),
@@ -77,8 +175,21 @@ class _scrollables2State extends State<scrollables2> {
                         width: 170,
                         height: 180,
                         decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10)),
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              "vato/images/img5.png",
+                              height: 150,
+                            ),
+                            Text(
+                              "Assurance",
+                              style: TextStyle(fontSize: 20),
+                            )
+                          ],
+                        ),
                       ),
                       SizedBox(
                         width: 20,
@@ -89,6 +200,18 @@ class _scrollables2State extends State<scrollables2> {
                         decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              "vato/images/img6.png",
+                              height: 150,
+                            ),
+                            Text(
+                              "Consultation",
+                              style: TextStyle(fontSize: 20),
+                            )
+                          ],
                         ),
                       ),
                     ],
@@ -96,30 +219,30 @@ class _scrollables2State extends State<scrollables2> {
                   SizedBox(
                     height: 20,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 170,
-                        height: 180,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Container(
-                        width: 170,
-                        height: 180,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                    ],
-                  ),
+                  // Row(
+                  //   mainAxisAlignment: MainAxisAlignment.center,
+                  //   children: [
+                  //     Container(
+                  //       width: 170,
+                  //       height: 180,
+                  //       decoration: BoxDecoration(
+                  //         color: Colors.white,
+                  //         borderRadius: BorderRadius.circular(10),
+                  //       ),
+                  //     ),
+                  //     SizedBox(
+                  //       width: 20,
+                  //     ),
+                  //     Container(
+                  //       width: 170,
+                  //       height: 180,
+                  //       decoration: BoxDecoration(
+                  //         color: Colors.white,
+                  //         borderRadius: BorderRadius.circular(10),
+                  //       ),
+                  //     ),
+                  //   ],
+                  // ),
                 ],
               ),
             )
